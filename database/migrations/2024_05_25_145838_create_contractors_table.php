e542b291d699dec1e5a104e72dfd280a06f5d2b8
Hassan Alwan <hsnwww@gmail.com>
2024-05-25 19:05:01 +0300

    Tender cloud laravel project

---
+++ database/migrations/2024_05_25_145838_create_contractors_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up()
+    {
+        Schema::create('contractors', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('city');
+            $table->string('address');
+            $table->string('industry');
+            $table->string('email')->unique();
+            $table->string('phone');
+            $table->string('mobile');
+            $table->integer('employees_count');
+            $table->date('established_at');
+            $table->timestamps();
+        });
+    }
+
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('contractors');
+    }
+};
---
+++ database/migrations/2024_05_25_145850_create_projects_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up()
+    {
+        Schema::create('projects', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('creator_id')->constrained('companies')->onDelete('cascade'); // إضافة حقل creator_id وربطه بجدول الشركات
+            $table->string('name');
+            $table->string('industry');
+            $table->text('description');
+            $table->date('release_date');
+            $table->date('submission_deadline');
+            $table->date('opening_date');
+            $table->decimal('document_fee', 10, 2);
+            $table->timestamps();
+        });
+    }
+
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('projects');
+    }
+};
